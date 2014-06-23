6a2551c9b5d4330c92348bee5b586a82649c165c
Miko Glace <miko89100@hotmail.fr>
2014-06-23 14:47:46 +0200

    Création front avec yeoman --> Angular, Bootstrap, Compass, Grunt, Karma

---
+++ market-front/app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('marketFrontApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
